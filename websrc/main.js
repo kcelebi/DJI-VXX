deff61630bde9b3bbcb7ba07c79ee3b1413e47db
Kaya Celebi <kayacelebi17@gmail.com>
2021-03-16 02:29:13 -0400

    slider labels done

--- websrc/main.js
+++ websrc/main.js
@@ -1,27 +1,34 @@
-let dji, vxx;
+let dji, vxx, dji_date, cvpos;
 
 function preload() {
     dji = loadJSON('prediction_data/dji_data.json')
-    vxx = loadJSON('prediction_data/vxx_data.json')
+    
+    //vxx = loadJSON('prediction_data/vxx_data.json')
 }
 
 function setup() {
-	createCanvas(1000,700);
-	dji_plot = new Plot("DJI", dji, [0, width], [0, height/2]);
-	dji_slider = createSlider(1, 100, 300);
-	dji_slider.position(width/10, 150);
+	let cnv = createCanvas(1000,700);
 
-	vxx_plot = new Plot("VXX", vxx, [0, width], [height/2, height]);
+	
+	cvpos = cnv.position();
+	dji_date = Object.keys(dji);
+	dji_plot = new Plot("DJI", dji, [0, width], [0, height], 250);
+	dji_slider = createSlider(1, 100, 300);
+	dji_slider.position(cvpos.x + 25, cvpos.y + 50);
+
+	/*vxx_plot = new Plot("VXX", vxx, [0, width], [height/2, height]);
 	vxx_slider = createSlider(1, 100, 300);
-	vxx_slider.position(width/10, height/2+150);
+	vxx_slider.position(width/10, height/2+150);*/
 
 }
 
 function draw() {
 	background(255);
 	dji_plot.drawPlot(dji_slider.value()/100);
-	vxx_plot.drawPlot(vxx_slider.value()/100);
 
+	textSize(16);
 	stroke(0);
-	line(0, height/2, width, height/2);
+	text("Date Range: " + dji_date[dji_plot.lowerbound] + " to Today", 25, 35);
+	//vxx_plot.drawPlot(vxx_slider.value()/100);
+
 }
--- websrc/plot.js
+++ websrc/plot.js
@@ -1,6 +1,6 @@
 class Plot {
 
-	constructor(name, plot_dat, x_lim, y_lim, ){
+	constructor(name, plot_dat, x_lim, y_lim, plotlim){
 		this.name = name;
 		this.plot_dat = plot_dat;
 		console.log("Initializing data: " + this.name);
@@ -8,6 +8,11 @@ class Plot {
 		this.length = this.dates.length;
 		this.x_lim = x_lim; //array of x lower/upper bound
 		this.y_lim = y_lim;
+
+		this.plotlim = plotlim;
+
+		this.lowerbound = 0;
+
 
 		this.y_max = 40000;
 
@@ -45,18 +50,26 @@ class Plot {
 		
 		var num_data = Math.round(this.length * pc);
 		var lb = this.length - num_data;
+		this.lowerbound = lb;
 		var curr_data = this.getDataRange(lb, this.length-1);
 
-		if(this.name == "VXX"){
-			console.log("Drawing plot " + this.name);
-	        console.log("Num data points: " + num_data);
-	        console.log("LB: " + lb);
-    	}
+		//console.log("Drawing plot " + this.name);
+        //console.log("Num data points: " + num_data);
+        //console.log("LB: " + lb);
+    	
 		stroke(0,155,0);
         strokeWeight(1);
 
-		for(var i=1; i < num_data; i++){
-			var line1 = [map(i-1, 0, num_data, this.x_lim[0], this.x_lim[1], true), map(curr_data[i-1], 0, this.y_max, this.y_lim[1], this.y_lim[0], true) ];
+        var dx = 0;
+        if(num_data > this.plotlim){
+        	dx = Math.round(num_data/this.plotlim);
+        }
+        else{
+        	dx = 1;
+        }//number of items on screen
+
+		for(var i=dx; i < num_data; i+=dx){
+			var line1 = [map(i-dx, 0, num_data, this.x_lim[0], this.x_lim[1], true), map(curr_data[i-dx], 0, this.y_max, this.y_lim[1], this.y_lim[0], true) ];
 			var line2 = [map(i, 0, num_data, this.x_lim[0], this.x_lim[1], true), map(curr_data[i], 0, this.y_max, this.y_lim[1], this.y_lim[0], true) ];
 			line(line1[0], line1[1], line2[0], line2[1]);
 		}
